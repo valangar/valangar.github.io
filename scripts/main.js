e97a9eb87c380bb3ff19ec99ed7bed9c14318c68
valangar <varsha.alangar@gmail.com>
2018-07-31 18:54:37 -0700

    added skills click feature

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,6 @@ var logo = d3.selectAll("#logo"),
 	
 //Initial setup
 
-
-
 //Adding event listeners
 d3.select("#profile-pic")
 	.on("mouseover", function(){
@@ -17,16 +15,40 @@ d3.select("#profile-pic")
 		my_dp.classed("hidden", true);
 	});
 	
-d3.selectAll(".split-text").on("mouseover", function(){
-								var color = "rgba("+(Math.random() * (256 - 1) + 1) + ","+(Math.random() * (256 - 1) + 1) + ","+(Math.random() * (256 - 1) + 1) + ")";
-								d3.select(this).style("color", color);    
-							})
-							.on("mouseout", function(){
-								d3.select(this).style("color", "#fff");    
-							});
-
-d3.selectAll(".chip").on("click", function(d){
-	if(selected_chips.indexOf(d.attr("id") >= 0)) selected_chips.push();
+d3.select("#skills-container").selectAll(".chip").on("click", function(d){
+	console.log("clicked");
+	let is_clicked = d3.select(this).classed("chip-clicked"),
+		id = d3.select(this).attr("id");
+	d3.select(this).classed("chip-clicked", !is_clicked);
+	if(is_clicked){
+		//remove skill from selected_chip array
+		selected_chips.splice(selected_chips.indexOf(id), 1);
+	}
+	else{
+		//add skill to selected_chip array
+		selected_chips.push(id);
+	}
+	if(selected_chips.length == 0){
+		resetFilter();
+	}
+	else{
+		d3.selectAll(".work-card").classed("hidden", true);
+		d3.selectAll("."+selected_chips.join(" ,.")).classed("hidden", false);
+		d3.select("#reset-btn").classed("hidden", false);
+		d3.selectAll("#work-pg-note").classed("hidden", true);
+	}
 });
 
+d3.select("#reset-btn").on("click", function(){
+	selected_chips = [];
+	d3.selectAll(".chip").classed("chip-clicked", false);
+	resetFilter();
+});
 
+//Functions
+function resetFilter(){
+	d3.selectAll(".work-card").classed("hidden", false);
+	d3.select("#reset-btn").classed("hidden", true);
+	d3.selectAll("#work-pg-note").classed("hidden", false);
+}
+	
